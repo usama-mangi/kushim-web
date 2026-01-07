7fa8b2a16250f314bd9e9e7f0b17c34b83ccb742
UsamaGM <usamapk7861@gmail.com>
2026-01-07 17:21:43 +0500

    fix: Enable CORS in API for frontend communication

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // Allow all origins for development
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
